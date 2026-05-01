e9d5404c6fc291b4ed9a15cf3e80ed2f1feb1dab
KarpovDenis74 <fotokarpov@yandex.ru>
2026-05-01 23:56:12 +0300

    Предварительная настройка проекта

---
+++ .storybook/main.ts
@@ -0,0 +1,29 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import type { StorybookConfig } from '@storybook/react-vite';
+import { mergeConfig } from 'vite';
+
+const storybookDir = path.dirname(fileURLToPath(import.meta.url));
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@chromatic-com/storybook',
+    '@storybook/addon-vitest',
+    '@storybook/addon-a11y',
+    '@storybook/addon-docs',
+    '@storybook/addon-onboarding',
+  ],
+  framework: '@storybook/react-vite',
+  async viteFinal(viteConfig) {
+    return mergeConfig(viteConfig, {
+      resolve: {
+        alias: {
+          '@': path.resolve(storybookDir, '../src'),
+        },
+      },
+    });
+  },
+};
+export default config;
---
+++ jest.config.cjs
@@ -0,0 +1,22 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  testEnvironment: 'jsdom',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(gif|ttf|eot|svg|png|jpg|jpeg|webp|avif)$':
+      '<rootDir>/src/test/mocks/fileMock.ts',
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/test/setupTests.ts'],
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.jest.json',
+      },
+    ],
+  },
+};
